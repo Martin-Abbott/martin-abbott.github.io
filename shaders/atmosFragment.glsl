c0bcfcdb66f62d12cffec63e805c62a72e7b5f4d
Martin Abbott <abbott01@googlemail.com>
2023-08-10 12:00:18 +0100

    Removed shaders due to glsl mime type issues

--- shaders/atmosFragment.glsl
+++
@@ -1,7 +0,0 @@
-varying vec3 vectorNormal;
-
-void main() {
-    float intensity = pow(1.5 - dot(vectorNormal, vec3(0.1, 0.3, 0.6)), 1.0);
-
-    gl_FragColor = vec4(0.1, 0.3, 0.8, 0.15) * intensity;
-}
