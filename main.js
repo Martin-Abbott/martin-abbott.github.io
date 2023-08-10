20b4e68907d0ec3786864b15270faf557a2d69f0
Martin Abbott <abbott01@googlemail.com>
2023-08-10 15:56:23 +0100

    revert to plain gsap import

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { gsap } from "./gsap.min.js";
+import { gsap } from "gsap";
 
 const dims = {
 	width: document.documentElement.clientWidth,
