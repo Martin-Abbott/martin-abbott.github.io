b4ae16d70385ced5c88f0a9ab077122131b75931
Martin Abbott <abbott01@googlemail.com>
2023-09-29 12:51:46 +0100

    added Google Analytics 4 to dependencies and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
@@ -6,7 +8,17 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+ReactGA.initialize("G-XYKKFRZLKV");
+
 const App = () => {
+	useEffect(() => {
+		ReactGA.send({
+			hitType: "pageview",
+			page: window.location.pathname + window.location.search,
+			title: "homePage",
+		});
+	}, []);
+
 	return (
 		<div>
 			<Nav />
